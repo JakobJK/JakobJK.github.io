1cfac94c02a05f4dc6ab3d95b9e837b68602002c
JakobJK <jakobjk@gmail.com>
2021-01-02 16:34:08 -0500

    wip

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
 const addPosts = () => {
-  posts = [
-    {
-      title: 'AWS Lambda for S3',
-      slug: 'aws-lambda-for-s3',
-      date: '01/02-2020',
-    },
-    {
-      title: 'Functions in PostgresQL',
-      slug: 'functions-in-postgresql',
-      date: '01/02-2020',
-    },
-    {
-      title: 'OpenVPN for secure access to Private Subnet',
-      slug: 'vpn-private-subnet',
-      date: '01/02-2020',
-    },
-  ]
-
-  const unorderedList = document.createElement('ul')
+  const li = document.createElement('ul')
 
   posts.forEach(post => {
     const listItem = document.createElement('li')
-    unorderedList.append(listItem)
+    li.append(listItem)
     listItem.innerHTML = `<a href="post/${post.slug}">${post.title}</a>`
   })
 
-  document.querySelector('#blog').append(unorderedList)
+  document.querySelector('#blog').append(li)
 }
 
 addPosts()
---
+++ post/posts.js
@@ -0,0 +1,18 @@
+const posts = [
+  {
+    title: 'AWS Lambda for S3',
+    slug: 'aws-lambda-for-s3',
+    date: '01/02-2020',
+    content: `<p>This is amazing. It totally kinda works! Does things line up if I keep adding a super long string</p>`,
+  },
+  {
+    title: 'Functions in PostgresQL',
+    slug: 'functions-in-postgresql',
+    date: '01/02-2020',
+  },
+  {
+    title: 'OpenVPN for secure access to Private Subnet',
+    slug: 'vpn-private-subnet',
+    date: '01/02-2020',
+  },
+]

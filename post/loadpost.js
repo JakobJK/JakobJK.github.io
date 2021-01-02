701e5f448a0d5611d06804d6082c117866d977ac
JakobJK <jakobjk@gmail.com>
2021-01-02 16:46:31 -0500

    yup

--- post/loadpost.js
+++ post/loadpost.js
@@ -1,12 +1,11 @@
 const addPost = () => {
   const contentElement = document.querySelector('#content')
   const pathSlug = document.location.pathname.split('/')[2]
-  console.log(pathSlug)
   const post = posts.find(x => x.slug === pathSlug)
 
   const { title, content, date } = post
   contentElement.innerHTML = `
-  <h1>${title}!!!</h1>
+  <h1>${title}</h1>
   <h3>${date}</h3>
   ${content}
   `

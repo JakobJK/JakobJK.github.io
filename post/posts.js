b8214d05f2850f5cfe2e9d86f42ede3d156198dc
JakobJK <jakobjk@gmail.com>
2021-01-03 02:12:26 -0500

    updates

--- post/posts.js
+++ post/posts.js
@@ -1,6 +1,12 @@
 const blogTitle = 'development blog'
 const posts = [
   {
+    title: 'NODE_ENV is not your environment',
+    slug: 'node-env-is-not-your-environment',
+    date: '01/02-2020',
+    content: '<p>This will be another brainfart - promise!</a>',
+  },
+  {
     title: 'AWS Lambda for S3',
     slug: 'aws-lambda-for-s3',
     date: '01/02-2020',
@@ -10,10 +16,12 @@ const posts = [
     title: 'Functions in PostgresQL',
     slug: 'functions-in-postgresql',
     date: '01/02-2020',
+    content: `<p>This is amazing. It totally kinda works! Does things line up if I keep adding a super long string</p>`,
   },
   {
-    title: 'OpenVPN for secure access to Private Subnet',
+    title: 'Connect to Private Subnets',
     slug: 'vpn-private-subnet',
     date: '01/02-2020',
+    content: `<p>This is amazing. It totally kinda works! Does things line up if I keep adding a super long string</p>`,
   },
 ]
